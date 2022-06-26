43ca8510897bd94cdebb4e5d1ab0ad79c9235071
talbara <mariobarasona@gmail.com>
2022-06-26 12:42:56 +0200

    Dinamic and Static URLS

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -3,6 +3,8 @@ import Link from 'next/link'
 import Layout from '../components/layout';
 import { getSortedPostsData } from '../lib/posts'
 
+//Me he quedado en getStaticProps Details con Next.js
+
 export async function getStaticProps(){
   const allPostsData = getSortedPostsData();
   return {
